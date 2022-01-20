829e29eef657a6457df44561d2787fbb708b7260
Andres Lince Pineda <andres.lince@globant.com>
2022-01-20 11:28:17 -0500

    00005 - Cracking the coding interview Exercise 1.2 Check permutation

--- app/Models/Chapter1.php
+++ app/Models/Chapter1.php
@@ -25,5 +25,36 @@ class Chapter1
 
         return true;
     }
+
+    /**
+     * Function that determines if a string is a permutation of other string
+     * 
+     * @param string $string1 string to validate if is a permutation of string2
+     * 
+     * @param string $string2 string to validate if string1 is a permutation of this
+     * 
+     * @return bool
+     * 
+     */
+    public static function isPermutation(string $string1, string $string2): bool{
+        $length1 = strlen($string1);
+        $length2 = strlen($string2);
+        if ($length1 != $length2) {
+            return false;
+        }
+
+        $arrayString1 = [];
+        for ($i=0; $i < $length1; $i++){
+            $arrayString1[$string1[$i]] = 1;
+        }
+
+        for ($i = 0; $i < $length2; $i++) {
+            if(!isset($arrayString1[$string2[$i]])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 ?>
