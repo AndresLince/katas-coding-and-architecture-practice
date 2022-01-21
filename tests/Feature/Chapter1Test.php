a6c977aa5c09d49a0a2b62e2966e2da7860bfc2c
Andres Lince Pineda <andres.lince@globant.com>
2022-01-21 16:59:05 -0500

    00006 - Tests of Cracking the coding interview Exercise 1.3 URLify

--- tests/Feature/Chapter1Test.php
+++ tests/Feature/Chapter1Test.php
@@ -34,5 +34,13 @@ class Chapter1Test extends TestCase
         $validation = Chapter1::isPermutation($string1, $string2);
         $this->assertEquals($validation,false);
     }
+
+    /** @test */
+    public function is_correct_urlified() {
+        $string = 'Mr John Smith     ';
+        $string_result = 'Mr%20John%20Smith';
+        $result = Chapter1::urlify($string, 13);
+        $this->assertEquals($string_result, $result);
+    }
 }
 ?>
